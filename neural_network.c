3876adcd5deab527f46568f574aa6800bea140e2
Elijah Herring <eliherringonpc@gmail.com>
2026-06-06 02:22:02 -0500

    Worked on forward(), not entiretly sure if it works properly. It also needs to make sure to save the relevant info for backward().

--- neural_network.c
+++ neural_network.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <time.h>
 
 
 //--  --  --  --  --  --  --  Vectors  --  --  --  --  --  --  --//
@@ -82,6 +84,19 @@ Matrix mat_create(size_t rows, size_t cols){
     return m;
 }
 
+Matrix mat_create_random(size_t rows, size_t cols){
+    Matrix m;
+    m.rows = rows;
+    m.cols = cols;
+    m.size = rows * cols;
+    m.data = calloc(m.size, sizeof(double));
+    Matrix *p = &m;
+    for(size_t i = 0; i < p->size; i++){
+        p->data[i] = (double)rand() / ((double)RAND_MAX + 1.0);
+    }
+    return m;
+}
+
 void mat_free(Matrix *m){
     free(m->data);
     m->data = NULL;
@@ -93,14 +108,17 @@ void mat_copy(Matrix *dst, const Matrix *src){
     memcpy(dst->data, src->data, src->size * sizeof(double));
 }
 
-void mat_add(Matrix *dst, const Matrix *a, const Matrix *b){
+Matrix mat_add(const Matrix *a, const Matrix *b){
+    Matrix m = mat_create(a->rows, a->cols);
+    Matrix *p = &m;
     if(a->rows == b->rows && a->cols == b->cols){
         for(size_t i = 0; i < (a->size); i++){
-            dst->data[i] = a->data[i] + b->data[i];
+            p->data[i] = a->data[i] + b->data[i];
         }
     } else {
         printf("Cannot add matrices - dimension mismatch.\n");
     }
+    return m;
 }
 
 void mat_hadamard(Matrix *dst, const Matrix *a, const Matrix *b){
@@ -110,6 +128,12 @@ void mat_hadamard(Matrix *dst, const Matrix *a, const Matrix *b){
         }
     } else {
         printf("Cannot take the hadamard product of matrices - dimension mismatch.\n");
+    }
+}
+
+void mat_apply(Matrix *m, double (*fn)(double)){
+    for(size_t i = 0; i < m->size; i++){
+        m->data[i] = fn(m->data[i]);
     }
 }
 
@@ -139,14 +163,6 @@ double mat_mult_sum_helper(const Matrix *a, const Matrix *b, size_t i, size_t j)
 }
 
 Matrix mat_multiply(Matrix *a, Matrix *b){
-
-    //dimension check here.
-
-    //needs to consider what the size of the dst matrix should be.
-    //Probably should be allocated here,
-    //Can be returned as a matrix object. 
-    //Whatever calls this will have to be in charge of freeing it. 
-
     Matrix m = mat_create(a->rows, b->cols);
     Matrix *p = &m;
 
@@ -196,45 +212,19 @@ void mat_print(Matrix *m) {
     printf("\n");
 }
 
-void test_mat(){
-    Matrix m1 = mat_create(2,5);
-    Matrix *p1 = &m1;
 
-    Matrix m2 = mat_create(5,2);
-    Matrix *p2 = &m2;
+//--  --  --  --  --  --  --  Functions  --  --  --  --  --  --  --//
 
 
-    for(size_t i = 0; i < p1->size; i++){
-        p1->data[i] = (i + 1);
-    }
-
-    for(size_t i = 0; i < p2->size; i++){
-        p2->data[i] = (10 - i);
-    }
-
-
-    
-    mat_print(p1);    
-    mat_print(p2);
-
-    Matrix m3 = mat_multiply(p1,p2);
-    Matrix *p3 = &m3;
-
-    mat_print(p3);
-
-    mat_transpose(p3);
-
-    mat_print(p3);
-
-    mat_free(p1);
-    mat_free(p2);
-    mat_free(p3);
+double sigmoid(double x){
+    return 1.0 / (1 + exp(-x));
 }
 
 
 //--  --  --  --  --  --  --  Neural Network  --  --  --  --  --  --  --//
 
 
+//-- -- -- -- 2 -> 2 -> 1 -- -- -- --//
 typedef struct {
     Matrix W1;
     Matrix b1;
@@ -249,6 +239,48 @@ typedef struct {
     Matrix a2;
 } Network;
 
+Network create_network(){
+    Network n;
+
+    n.W1 = mat_create_random(2,2);
+    n.b1 = mat_create_random(2,1);
+
+    n.W2 = mat_create_random(1,2);
+    n.b2 = mat_create_random(1,1);
+
+    return n;
+}
+
+//wow this looks absolutely garbage...
+//Need to save these somewhere for later for backpropogation
+//Might be worth using another external file to do so. 
+//Reading and writing is slow though... might make the imbedded system idea wack. 
+double forward(Network *n, Matrix *input){
+    Matrix *W1 = &n->W1;
+    Matrix temp = mat_multiply(W1, input);
+    Matrix *b1 = &n->b1;
+    n->z1 = mat_add(&temp, b1);
+    mat_free(&temp);
+    
+    Matrix temp2 = n->z1;
+    mat_apply(&temp2, sigmoid);
+    n->a1 = temp2;
+
+    Matrix *W2 = &n->W2;
+    Matrix temp3 = mat_multiply(W2, &temp2);
+    Matrix *b2 = &n->b2;
+    n->z2 = mat_add(&temp3, b2);
+    mat_free(&temp3);
+    mat_free(&temp2);
+
+    Matrix temp4 = n->z2;
+    mat_apply(&temp4, sigmoid);
+    n->a2 = temp4;
+    mat_free(&temp4);
+
+    //I think a2 is just a double atp... return it?
+    return (double)n->a2.data[0];
+}   
 
 
 //--  --  --  --  --  --  --  Testing  --  --  --  --  --  --  --//
@@ -286,10 +318,47 @@ void test_vec(){
     printf("------------vec test over------------\n");
 }
 
-void test_mat(int temp){
-    printf("--------------mat test--------------\n");
+void test_mat(){
+    Matrix m1 = mat_create(2,5);
+    Matrix *p1 = &m1;
+
+    Matrix m2 = mat_create(5,2);
+    Matrix *p2 = &m2;
+
+
+    for(size_t i = 0; i < p1->size; i++){
+        p1->data[i] = (i + 1);
+    }
+
+    for(size_t i = 0; i < p2->size; i++){
+        p2->data[i] = (10 - i);
+    }
+
+
     
-    printf("------------mat test over------------\n");
+    mat_print(p1);    
+    mat_print(p2);
+
+    Matrix m3 = mat_multiply(p1,p2);
+    Matrix *p3 = &m3;
+
+    Matrix m4 = mat_create_random(2,2);
+    Matrix *p4 = &m4;
+    mat_print(p4);
+
+    mat_print(p3);
+
+    mat_transpose(p3);
+
+    mat_print(p3);
+
+    mat_apply(p3, sigmoid);
+
+    mat_print(p3);
+
+    mat_free(p1);
+    mat_free(p2);
+    mat_free(p3);
 }
 
 
@@ -297,6 +366,34 @@ void test_mat(int temp){
 
 
 void main(int argc, char *argv[]){
-    test_vec();
-    test_mat();
+    srand(time(NULL)); //update to time(NULL) later. 
+    //test_vec();
+    //test_mat();
+    Network n = create_network();
+    Matrix a = mat_create(2,1);
+    a.data[0] = 0;
+    a.data[1] = 0;
+
+    Matrix b = mat_create(2,1);
+    b.data[0] = 0;
+    b.data[1] = 1;
+
+    Matrix c = mat_create(2,1);
+    c.data[0] = 1;
+    c.data[1] = 0;
+
+    Matrix d = mat_create(2,1);
+    d.data[0] = 1;
+    d.data[1] = 1;
+
+
+    double value1 = forward(&n, &a);
+    double value2 = forward(&n, &b);
+    double value3 = forward(&n, &c);
+    double value4 = forward(&n, &d);
+
+    printf("Prediction: XOR(0,0) ~ %.4f\n", value1);
+    printf("Prediction: XOR(0,0) ~ %.4f\n", value2);
+    printf("Prediction: XOR(0,0) ~ %.4f\n", value3);
+    printf("Prediction: XOR(0,0) ~ %.4f\n", value4);
 }
